268738f99902b600a26bf062daf20b40df9434d3
Harshit <itsharshitraj@gmail.com>
2025-03-04 13:57:01 +0530

    [Harshit]Add . UC3-Restrict PIN code from ending with alphabets or special characters

--- pinCodeRegex.js
+++ pinCodeRegex.js
@@ -5,4 +5,5 @@ const testPinCode = (pin) => {
 };
 
 console.log(testPinCode("400088")); // true
-console.log(testPinCode("A400088")); // false
+console.log(testPinCode("400088B")); // false (should not end with an alphabet)
+console.log(testPinCode("400088#")); // false (should not end with a special character)
